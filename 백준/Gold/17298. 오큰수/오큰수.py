eca31174b8dd2e5677e26b1cf3d22f6f9f54ad9c
codus <97906653+rkdcodus@users.noreply.github.com>
2024-03-16 17:28:54 +0900

    [Gold IV] Title: 오큰수, Time: 1132 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,17 +1,16 @@
 n = int(input())
-num_list = input().split()
+num_list = list(map(int, input().split()))
 stack = []
 result = [-1]*n
 for i in range(n):
     while stack:
-        if int(stack[-1][0]) < int(num_list[i]):
-            result[stack[-1][1]] = num_list[i]
-            stack.pop()
+        if num_list[stack[-1]] < num_list[i]:
+            index = stack.pop()
+            result[index] = num_list[i]
         else:
-            stack.append([num_list[i], i])
+            stack.append(i)
             break
     if not stack:
-        stack.append([num_list[i], i])
+        stack.append(i)
 
 print(*result)
-

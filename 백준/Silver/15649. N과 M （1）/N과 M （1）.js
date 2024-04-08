321b17f1937f13bec4dd28520921c5d5d899274c
codus <97906653+rkdcodus@users.noreply.github.com>
2024-04-09 01:46:14 +0900

    [Silver III] Title: N과 M (1), Time: 2464 ms, Memory: 13532 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,25 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const n = input[0];
+const m = input[1];
+const result = [];
+const visited = new Array(n).fill(0);
+
+const backtracking = () => {
+  if (result.length === m) return console.log(result.join(" "));
+  for (let i = 1; i <= n; i++) {
+    if (visited[i]) continue;
+    visited[i] = 1;
+    result.push(i);
+    backtracking();
+    result.pop();
+    visited[i] = 0;
+  }
+};
+
+backtracking();

48345fc34303aeaa72a71bc3838d7aa2a92f2128
codus <97906653+rkdcodus@users.noreply.github.com>
2024-04-10 22:58:43 +0900

    [Silver III] Title: N과 M (3), Time: 1988 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -3,14 +3,11 @@ def backtracking():
         print(*result)
         return
     for i in range(1, n+1):
-        visited[i] = 1
         result.append(i)
         backtracking()
         result.pop()
-        visited[i] = 0
 
 n, m = map(int, input().split())
 
 result = []
-visited = [0] * (n+1)
 backtracking()

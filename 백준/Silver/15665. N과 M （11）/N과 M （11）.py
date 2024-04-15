873e03b2d7c23ed981700e5d1ab1979f3b3c5b0a
codus <97906653+rkdcodus@users.noreply.github.com>
2024-04-15 18:29:46 +0900

    [Silver II] Title: N과 M (11), Time: 748 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,21 @@
+def backtracking():
+    if len(result) == m:
+        print(*result)
+        return
+    prev = 0
+    for i in range(0, n):
+        if value[i] == prev:
+            continue
+        s[i] = 1
+        prev = value[i]
+        result.append(value[i])
+        backtracking()
+        result.pop()
+        s[i] = 0
+
+n, m = map(int, input().split())
+value = list(map(int, input().split()))
+s = [0] * n
+value.sort()
+result = []
+backtracking()

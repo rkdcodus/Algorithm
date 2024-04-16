e2c11679520bbdcbf3c8c736d837ead6f7c739e5
codus <97906653+rkdcodus@users.noreply.github.com>
2024-04-16 21:56:18 +0900

    [Gold V] Title: 암호 만들기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,45 +1,38 @@
 import sys
 input = sys.stdin.readline
 
-def bfs():
-    global vowel_count, consonant
+def bfs(idx):
+    global vowel, consonant
 
-    if vowel_count > 0 and consonant > 1 and len(result) == l:
+    if vowel > 0 and consonant > 1 and len(result) == l:
         print(''.join(result))
         return
 
-    for i in range(c):
-        if v[i]:
-            continue
+    for i in range(idx,c):
         if result and result[-1] > c_list[i]:
             continue
 
-        v[i] = 1
         result.append(c_list[i])
 
-        if c_list[i] in vowel:
-            vowel_count += 1
+        if c_list[i] in ['a', 'e', 'i', 'o', 'u']:
+            vowel += 1
         else:
             consonant += 1
 
-        bfs()
+        bfs(i+1)
 
-        v[i] = 0
         result.pop()
 
-        if c_list[i] in vowel:
-            vowel_count -= 1
+        if c_list[i] in ['a', 'e', 'i', 'o', 'u']:
+            vowel -= 1
         else:
             consonant -= 1
 
 l, c = map(int, input().split())
-c_list = input().split()
+c_list = sorted(input().split())
 
-c_list.sort()
-v = [0] * c
-vowel = ['a', 'e', 'i', 'o', 'u']
-vowel_count = 0
+vowel = 0
 consonant = 0
 result = []
 
-bfs()
+bfs(0)

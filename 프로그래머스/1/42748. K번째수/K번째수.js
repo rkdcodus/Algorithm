64a90d8cf2a5d1acaf89e5334ce159597e50128a
codus <97906653+rkdcodus@users.noreply.github.com>
2024-06-15 00:58:41 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    const answer = []
+    commands.forEach(element=>{
+        const [i, j, k] = element;
+        const sortedArr = array.slice(i-1, j).sort((a, b) => a - b)
+        answer.push(sortedArr[k-1])
+    })
+    return answer;
+}

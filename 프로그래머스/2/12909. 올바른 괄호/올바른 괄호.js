3eddac51ef193d72d76c0533f898e14dce37fe67
codus <97906653+rkdcodus@users.noreply.github.com>
2024-08-23 20:31:02 +0900

    [level 2] Title: 올바른 괄호, Time: 25.63 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,20 @@
+function solution(s){
+    let answer = true;
+    const arr_s = s.split('');
+    const stack = [];   
+    
+    for(let i = 0; i<arr_s.length; i++) {
+        if(arr_s[i] === '(') stack.push(i)
+        else if(arr_s[i] === ')') {
+            if(!stack.length) {
+                answer = false
+                break;
+            };
+            stack.pop();
+         }
+    }
+    
+    if(stack.length) answer = false;
+    
+    return answer;
+}

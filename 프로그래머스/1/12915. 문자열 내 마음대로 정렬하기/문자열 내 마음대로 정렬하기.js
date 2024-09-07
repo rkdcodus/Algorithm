684dea69858528c9df4508bf9980563f9f0a7963
codus <97906653+rkdcodus@users.noreply.github.com>
2024-09-07 19:56:48 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 8.77 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,7 +1,16 @@
+// 음수가 리턴된다면 a < b를 의미하고,
+// 양수가 리턴된다면 a > b를 의미하고,
+// 0이 리턴된다면 a == b를 의미한다.
+
 function solution(strings, n) {
-    return strings.sort().sort((a, b) => {
-        if( a[n] > b[n]) return 1
-        if(a[n] < b[n]) return -1
-        return 0
-    });
+  // strings.sort((a, b) => {
+  //   if (a[n] > b[n]) return 1;
+  //   if (a[n] < b[n]) return -1;
+  //   return a < b ? -1 : a > b ? 1 : 0;
+  // });
+    strings.sort((a, b) => {
+        if(a[n] === b[n]) return a.localeCompare(b);
+        return a[n].localeCompare(b[n]);
+    })
+  return strings;
 }

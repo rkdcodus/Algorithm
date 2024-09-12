427695c2f1aa7e867434f782364cc6dc4c2ea205
codus <97906653+rkdcodus@users.noreply.github.com>
2024-09-13 05:04:25 +0900

    [level 3] Title: 기지국 설치, Time: 2.29 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,17 +1,15 @@
 function solution(n, stations, w) {
-  const newStations = [];
-  let first = 0;
-  let answer = 0;
-  let index = 0;
+    const newStations = [];
+    let first = 0;
+    let answer = 0;
     
-  while(index < stations.length) {
-      const last = stations[index] - w - 1;
-      answer += Math.ceil((last-first) / (w * 2 + 1));
-      first = stations[index] + w;
-      index += 1;
+    for(let i=0;i<stations.length;i++) {
+        const last = stations[i] - w - 1;
+        answer += Math.ceil((last-first) / (w * 2 + 1));
+        first = stations[i] + w;
+    }
+  
+    if (first <= n) answer += Math.ceil((n - first) / (w * 2 + 1));
+  
+    return answer;
   }
-
-  if (first <= n) answer += Math.ceil((n - first) / (w * 2 + 1));
-
-  return answer;
-}

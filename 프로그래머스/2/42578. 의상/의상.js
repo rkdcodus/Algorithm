cf5c8a3209801f4ef194603a7f4251f6af880321
codus <97906653+rkdcodus@users.noreply.github.com>
2025-05-27 19:56:32 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,20 +1,14 @@
 function solution(clothes) {
     const map = new Map();
-    const types = [];
     let result = 1;
     
-    clothes.forEach((clothe) => {
-        const [name, type] = clothe
+    clothes.forEach(([_, type]) => {
         map.set(type, (map.get(type)|| 0) + 1)
-        if(!types.includes(type)) {
-            types.push(type)
-        }
     })
-    
-    
-    types.forEach((type) => {
-       result *= map.get(type) + 1
-    })
+ 
+    for(const count of map.values()) {
+        result *= (count + 1)
+    }
     
     return result - 1;
 }

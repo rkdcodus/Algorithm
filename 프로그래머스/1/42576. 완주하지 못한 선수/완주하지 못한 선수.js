3799eced8856d949222d06a73be34712a93b942a
codus <97906653+rkdcodus@users.noreply.github.com>
2026-02-07 15:17:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.06 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,19 @@
+// 존재 여부 확인 => map
+// 동명이인이 존재 => map 생성 시 같은 key, value를 가진 쌍 생성 불가. 
+// participant에서 map 객체 생성, value 값은 이름을 가진 사람 수로 저장. 
+// completion으로 map에 존재하다면 value 값을 -1 
+// 마지막에 map value값이 0이 아닌 사람이 완주 못한 사람..
 function solution(participant, completion) {
-   const map = new Map();
+    var answer = '';
+    const map = new Map();
     
-    for(const name of participant) {
-        map.set(name, (map.get(name) || 0) + 1)
+    for(const x of participant) map.set(x, (map.get(x) || 0) + 1);
+    
+    for(const y of completion) map.set(y, map.get(y) - 1);
+    
+    for(const [key, value] of map) {
+        if(value > 0) answer = key
     }
     
-    for(const name of completion) {
-        map.set(name, map.get(name) - 1)
-    }
-    
-    for(const [name, count] of map) {
-        if(count > 0) return name;
-    }
+    return answer;
 }

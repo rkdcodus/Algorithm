9e71c861b94a4032dbeecb682a4173ac3b0fde2a
codus <97906653+rkdcodus@users.noreply.github.com>
2026-02-07 21:43:09 +0900

    [level 1] Title: K번째수, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,14 @@
+// 자르기 slice k, 정렬 sort -> nlogn
 function solution(array, commands) {
-    const answer = []
-    commands.forEach(element=>{
-        const [i, j, k] = element;
-        const sortedArr = array.slice(i-1, j).sort((a, b) => a - b)
-        answer.push(sortedArr[k-1])
-    })
+    var answer = [];
+    
+    for(const x of commands) {
+        const [i, j, k] = x;
+        const slicedArr = array.slice(i-1, j);
+        slicedArr.sort((a,b) => a - b);
+        
+        answer.push(slicedArr[k-1]);
+    }
+    
     return answer;
 }
